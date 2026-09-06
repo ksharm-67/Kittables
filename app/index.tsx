f42e2038c131ef1b21d9d2bc1529644a5b94baab
Your Name <you@example.com>
2026-09-06 04:43:27 -0700

    initial commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,19 @@
 import { registerRootComponent } from 'expo';
+import App from './app';
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 
-import App from './App';
+const index = () => {
+  return (
+    <View>
+      <Text>index</Text>
+    </View>
+  )
+}
 
+export default index
+
+const styles = StyleSheet.create({})
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
