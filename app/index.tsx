e300ae427b8cbbf0f424d040100b13351f36490a
Your Name <you@example.com>
2026-09-21 03:00:11 -0700

    Update login screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,7 @@
 import { registerRootComponent } from 'expo';
 import { useFonts, Nunito_400Regular, Nunito_700Bold } from '@expo-google-fonts/nunito';
 import { StyleSheet, Text, View } from 'react-native'
+import { Button, withTheme } from '@rneui/themed';
 import App from './app';
 import React from 'react'
 
@@ -14,9 +15,30 @@ const index = () => {
 
   return (
     <View style={styles.container}>
+      <Button
+        style={{
+          position: 
+        }}
+        title="Log On"
+        titleStyle={styles.titStyle}
+        buttonStyle={styles.butStyle}
+        containerStyle={{
+          width: 90,
+          marginHorizontal: 50,
+          marginVertical: 10,
+        }}
+      />
+
       <Text style={styles.text}>
         Welcome to the Kittables app!
       </Text>
+
+      <Button
+        title="Browse Cats"
+        titleStyle={styles.titStyle}
+        buttonStyle={styles.butStyle}
+        containerStyle={styles.contStyle}
+      />
     </View>
   )
 }
@@ -34,7 +56,21 @@ const styles = StyleSheet.create({
     fontFamily: 'Nunito_400Regular',
     fontSize: 20,
   },
+  titStyle: {
+    fontFamily: 'Nunito_700Bold',
+    fontSize: 16,
+    color: 'black',
+  },
+  butStyle: {
+    backgroundColor: '#b6e6ff',
+    borderRadius: 15,
+  },
+  contStyle: {
+    width: 150,
+    marginHorizontal: 50,
+    marginVertical: 10,
+  },
 })
 
-registerRootComponent(App);
+registerRootComponent(index);
 
